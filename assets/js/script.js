f73a5876b8c6e29a95b88e34eef47358ceaece59
Indah Nur Fauziah <indahnurfauziah731@gmail.com>
2023-09-17 17:52:26 +0700

    penambahan

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,3 +5,26 @@ function showMenu(){
 function hideMenu(){
     navLinks.style.top = '-1000px'
 }
+// Modal Box
+const itemDetailModal = document.querySelector('#item-detail-modal');
+const itemDetailButtons = document.querySelectorAll('.item-detail-button');
+
+itemDetailButtons.forEach((btn) => {
+  btn.onclick = (e) => {
+    itemDetailModal.style.display = 'flex';
+    e.preventDefault();
+  };
+});
+
+// klik tombol close modal
+document.querySelector('.modal .close-icon').onclick = (e) => {
+  itemDetailModal.style.display = 'none';
+  e.preventDefault();
+};
+
+// klik di luar modal
+window.onclick = (e) => {
+  if (e.target === itemDetailModal) {
+    itemDetailModal.style.display = 'none';
+  }
+};
