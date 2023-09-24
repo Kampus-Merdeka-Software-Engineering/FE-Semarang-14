a7de90e0c12628f8f3c89135acc9d79bd4de062b
M. Khoirul Ma'arif <maarif2910@students.undip.ac.id>
2023-09-24 22:12:20 +0700

    Add slider

---
+++ assets/js/about.js
@@ -0,0 +1,80 @@
+// Slider
+function handleScreenWidth() {
+    const screenWidth = window.innerWidth;
+    console.log("Screen Width:", screenWidth);
+    if (screenWidth <= 768) {
+        let currentSlide = 0;
+    
+        const prevBtn = document.getElementById("prevBtn");
+        const nextBtn = document.getElementById("nextBtn");
+    
+        prevBtn.addEventListener("click", () => {
+            showSlide(currentSlide - 1);
+        });
+    
+        nextBtn.addEventListener("click", () => {
+            showSlide(currentSlide + 1);
+        });
+    
+        function showSlide(n) {
+            const slides = document.querySelectorAll(".card");
+    
+            if (n < 0) {
+            currentSlide = slides.length - 1;
+            } else if (n >= slides.length) {
+            currentSlide = 0;
+            } else {
+            currentSlide = n;
+            }
+    
+            slides.forEach((slide, index) => {
+            if (index === currentSlide) {
+                slide.style.display = "block";
+            } else {
+                slide.style.display = "none";
+            }
+            });
+        }
+    
+        window.addEventListener("load", showSlide(currentSlide));
+    } else {
+        const cards = document.querySelectorAll(".card");
+        const prevBtn = document.getElementById("prevBtn");
+        const nextBtn = document.getElementById("nextBtn");
+        const cardsPerPage = 3;
+        let currentIndex = 0;
+    
+        function showCards() {
+            cards.forEach((card, index) => {
+            card.style.display = "none";
+            const adjustedIndex =
+                (index + cards.length - currentIndex) % cards.length;
+            if (adjustedIndex >= 0 && adjustedIndex < cardsPerPage) {
+                card.style.display = "block";
+            }
+            });
+        }
+    
+        function goToNextPage() {
+            currentIndex = (currentIndex + 1) % cards.length;
+            showCards();
+        }
+    
+        function goToPrevPage() {
+            currentIndex = (currentIndex - 1 + cards.length) % cards.length;
+            showCards();
+        }
+    
+        nextBtn.addEventListener("click", goToNextPage);
+        prevBtn.addEventListener("click", goToPrevPage);
+    
+        window.addEventListener("load", showCards());
+        window.addEventListener("load", goToNextPage());
+        window.addEventListener("load", goToPrevPage());
+    }
+}
+  
+window.addEventListener("resize", handleScreenWidth);
+
+handleScreenWidth();
+  
