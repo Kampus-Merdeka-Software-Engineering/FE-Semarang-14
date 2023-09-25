a3513c16513551400d67523ca1e3c54629e122ce
M. Khoirul Ma'arif <maarif2910@students.undip.ac.id>
2023-09-25 21:51:54 +0700

    fix pop-up modal

--- assets/js/course.js
+++ assets/js/course.js
@@ -1,6 +1,7 @@
 var modal = document.getElementById("myModal");
 var btn = document.getElementById("myBtn");
 var span = document.getElementsByClassName("close")[0];
+
 document.addEventListener("click", function (event) {
     if (event.target.classList.contains("myBtn")) {
     modal.style.display = "block";
@@ -16,16 +17,3 @@ window.onclick = function (event) {
     modal.style.display = "block";
     }
 };
-
-let navLinks = document.getElementById("navLinks");
-function showMenu() {
-    navLinks.style.top = "0";
-}
-function hideMenu() {
-    navLinks.style.top = "-1000px";
-}
-
-document.querySelector(".submit-email").addEventListener("mousedown", (e) => {
-    e.preventDefault();
-    document.querySelector(".subscription").classList.add("done");
-});
