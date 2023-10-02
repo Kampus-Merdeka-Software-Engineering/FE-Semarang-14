878f509f3e1062071b0f97f039e983e49e00f46b
M. Khoirul Ma'arif <maarif2910@students.undip.ac.id>
2023-10-02 18:31:55 +0700

    add modal to index

--- assets/js/index.js
+++ assets/js/index.js
@@ -13,3 +13,24 @@ submit.addEventListener("mousedown", (e) => {
         subscription.classList.remove("error");
     }
 });
+
+// Modal handling
+var modal = document.getElementById("myModal");
+var btn = document.getElementById("myBtn");
+var span = document.getElementsByClassName("close")[0];
+
+document.addEventListener("click", function (event) {
+    if (event.target.classList.contains("myBtn")) {
+    modal.style.display = "block";
+    }
+});
+
+span.onclick = function () {
+    modal.style.display = "none";
+};
+
+window.onclick = function (event) {
+    if (event.target == modal) {
+    modal.style.display = "block";
+    }
+};
