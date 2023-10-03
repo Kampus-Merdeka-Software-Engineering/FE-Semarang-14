a49ea0b52ec6d0b52104777f3fc37218baf77754
M. Khoirul Ma'arif <maarif2910@students.undip.ac.id>
2023-10-03 12:13:47 +0700

    add dynamic modal

--- assets/js/courses.js
+++ assets/js/courses.js
@@ -26,7 +26,7 @@ window.onload = async () => {
                 <div class="course-col">
                     <div>
                         <div class="course-image">
-                            <img src="./assets/images/course-image5.png" alt="" />
+                            <img src="./assets/images/${course.photo}" alt="" />
                         </div>
                         <div class="course-tag">
                             <h4>${course.nama}</h4>
@@ -45,7 +45,7 @@ window.onload = async () => {
                         </div>
                         </div>
                     <div class="course-btn">
-                        <a class="myBtn">Join Course</a>
+                        <a class="myBtn" onclick="showModal('${course.id}')">Join Course</a>
                     </div>
                 </div>
                 `;
@@ -61,21 +61,92 @@ window.onload = async () => {
 
 // Modal Handling
 var modal = document.getElementById("myModal");
-var btn = document.getElementById("myBtn");
-var span = document.getElementsByClassName("close")[0];
 
-document.addEventListener("click", function (event) {
-    if (event.target.classList.contains("myBtn")) {
+function showModal() {
     modal.style.display = "block";
-    }
-});
+}
 
-span.onclick = function () {
+// show modal by id
+function showModal(id) {
+    modal.style.display = "block";
+    console.log(id);
+
+    fetch(`${BASE_URL}/courses/${id}`, {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json",
+        },
+    }).then((response) => {
+        response.json().then((data) => {
+            let course = data;
+            // console.log(course);
+
+            var modalTemplate = `
+            <div class="modal-course">
+                <div class="modal-course-col">
+                    <div class="modal-close">
+                        <span class="close" onclick="closeModal()">&times;</span>
+                        <h3>Join Our Class</h3>
+                    </div>
+
+                    <div>
+                        <div class="modal-course-image">
+                            <img src="./assets/images/${course.photo}" alt="" />
+                        </div>
+                        <div class="modal-course-tag">
+                            <h4>${course.nama}</h4>
+                        </div>
+                        <div class="modal-course-title-rating">
+                            <h3>${course.nama}</h3>
+                            <h3>${course.rating}/10</h3>
+                        </div>
+                        <div class="modal-course-price">
+                            <h4>${NumFormatter.format(course.harga)}</h4>
+                        </div>
+                        <div class="modal-course-text">
+                            <p>${course.deskripsi_panjang}</p>
+                        </div>
+                    </div>
+                    <div class="modal-content">
+                    <h2>Contact Form</h2>
+                        <form id="contactForm">
+
+                            <div class="form-label">
+                                <label for="name">Name <span>:</span></label>
+                                <input type="text" id="name" name="name" required>
+                            </div>
+
+                            <div class="form-label">
+                                <label for="email">Email <span>:</span></label>
+                                <input type="email" id="email" name="email" required>
+                            </div>
+
+                            <div class="form-label">
+                                <label for="phone">Phone Number <span>:</span></label>
+                                <input type="tel" id="phone" name="phone" required>
+                            </div>
+
+                            <button type="submit">Send <i class="fa-solid fa-arrow-right"></i></button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+            `;
+
+            // console.log(modalTemplate);
+            modal.innerHTML = modalTemplate;
+        });
+    }).catch((error) => {
+        console.log(error);
+    });
+}
+
+function closeModal() {
     modal.style.display = "none";
-};
+}
 
 window.onclick = function (event) {
     if (event.target == modal) {
-    modal.style.display = "block";
+        modal.style.display = "block";
     }
 };
